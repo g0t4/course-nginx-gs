c433d4fecc01c475c3b9942614e8f91c13d3acfb
Wes Higbee <wes.mcclure@gmail.com>
2023-11-15 14:05:12 -0600

    notes

--- static/nginx_org/build.sh
+++ static/nginx_org/build.sh
@@ -1,4 +1,5 @@
 trash ./out # safe delete
++# FYI this means you have to restart docker compose up to pick up changes b/c I am nuking bind mounted dir
 
 docker image build --output type=local,dest=out .
 
