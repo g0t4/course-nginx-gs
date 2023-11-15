22366bc8d3cfd6858514ac65bcabd7d17a8f5a47
Wes Higbee <wes.mcclure@gmail.com>
2023-11-15 14:53:14 -0600

    notes

--- static/nginx_org/build.sh
+++ static/nginx_org/build.sh
@@ -1,3 +1,4 @@
+# it's ok to remove this trash line (obviously if you don't have trash-cli installed don't use it)
 trash ./out/* # safe delete, only nested files so this doesn't mess up bind mount into compose container
 
 docker image build --output type=local,dest=out .
